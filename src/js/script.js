8d3f1e252d812af8857775019b36c7fce1d51c1e
qetiproject <ketideveloper@gmail.com>
2019-10-07 18:53:55 +0400

    fixed leftside

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// let header_navigation_link = document.getElementsByClassName('header_navigation_link');
+// console.log(header_navigation_link)
+// headerColor =() => {
+//     if(window.location.href === "http://localhost:3000/index.html") {
+//         header_navigation_link.style.color = 'red'
+//     }
+// }
+// headerColor();
