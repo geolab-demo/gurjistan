0e8abd33e49b57ce7c84d92bafa3dca673350653
qetiproject <ketideveloper@gmail.com>
2019-10-08 12:00:04 +0400

    bgImage style

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,12 @@
-// let header_navigation_link = document.getElementsByClassName('header_navigation_link');
-// console.log(header_navigation_link)
-// headerColor =() => {
-//     if(window.location.href === "http://localhost:3000/index.html") {
-//         header_navigation_link.style.color = 'red'
-//     }
-// }
-// headerColor();
+let header_navigation_link = document.getElementsByClassName('header_navigation_link');
+let right_side = document.getElementById('right_side');
+
+headerColor =() => {
+    if(window.location.href === "http://localhost:3000/index.html") {
+        for(let i=0; i < header_navigation_link.length; i++ ){
+            header_navigation_link[i].style.color = '#fff';
+        } 
+        right_side.classList.add("bgImage");
+    }
+}
+headerColor();
