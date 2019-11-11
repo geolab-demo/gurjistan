26103c82463f0cc78cb99d584a6268a6bc5f0346
keti khetsuriani <k.khetsuriani@betsolutions.com>
2019-11-11 10:50:06 +0400

    tabs&styles

--- src/js/search.js
+++ src/js/search.js
@@ -1,21 +1,20 @@
 if(checkPage('search')) {
     // search page
   let peopleList = document.querySelector('.search_content');
-  let searchAll = document.querySelector('.search');
   let alphabetList = document.querySelector('.alphabet');
   let alphabet = [];
   let people = [];
 
   getPeople();
   getAlphabet();
-searchAll.addEventListener('click', () =>{
-  console.log('jjj')
-})
+
    populatePeople = (peopleArr, fromSearch) =>{
     alphabet.forEach(i => {
-      if(!fromSearch) {        
+      if(!fromSearch) {      
         alphabetList.innerHTML += `
-          <li class="alphabetItem"><a onclick="filterPeopleWithAlphabet('${i.id}')" class="alphabet_${i.id}">${i.value}</a></li>`;
+          <li class="alphabetItem">
+            <a onclick="filterPeopleWithAlphabet('${i.id}')" class="alphabet_${i.id}">${i.value}</a>
+          </li>`;
       }
       peopleList.innerHTML += `
       <li class="search_content_list peopleList_${i.id}">${i.value}<ul class="people_${i.id}"></ul>
@@ -25,7 +24,8 @@ searchAll.addEventListener('click', () =>{
       let filteredPeople = peopleArr.filter( p => p.surname.slice(0, 1) === i.value);
       let maxResultCount = 10;
       let selectedbtn = document.querySelector(`.btn_${i.id}`);
-
+     
+      
       if(maxResultCount >= filteredPeople.length) {
         selectedbtn.style.display = 'none';
       }
@@ -47,13 +47,14 @@ searchAll.addEventListener('click', () =>{
     people = await response.json();
     alphabet.length ? populatePeople(people, false) : '';
   }
-  
    filterPeopleWithAlphabet = (id) => {
+   
     let selectedPeople = document.querySelector(`.peopleList_${id}`);
     let peopleContentList = document.querySelectorAll('.search_content_list');
     peopleContentList.forEach(i => {
       i.style.display = 'none';
     });
+   
     selectedPeople.style.display = 'block';
   }
 
